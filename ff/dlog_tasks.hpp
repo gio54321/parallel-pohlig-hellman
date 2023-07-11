9599ae94edc23cddbf319b4870feab5204228db3
gio54321 <g.dellimmagine@gmail.com>
2023-07-11 21:13:08 +0200

    Add comments and cleanup code of fastflow version

--- ff/dlog_tasks.hpp
+++ ff/dlog_tasks.hpp
@@ -2,46 +2,70 @@
 #include <vector>
 #include <atomic>
 
-
+/*
+ * Task strucure for the entire Pohlig-Hellman algorithm
+ */
 struct poh_task_t {
+    // input parameters
     mpz_class g;
     mpz_class b;
     mpz_class p;
     mpz_class order;
     std::vector<std::pair<mpz_class, mpz_class>> order_factorization;
 
+    // output vectors
     std::vector<mpz_class> x;
     std::vector<mpz_class> h_i;
 
+    // number of completed subgroups, i.e., number of completed number of completed Pohlig-Hellman
     int completed_subgroups;
 };
 
+/*
+ * Task structure for one computation of Pohlig-Hellman on a group of prime power order
+*/
 struct poh_pp_task_t {
+    // input parameters, group order = factor^exponent
     mpz_class g;
     mpz_class b;
     mpz_class factor;
     mpz_class exponent;
-    mpz_class iteration;
+
+    // computed gamma for the task
     mpz_class gamma;
 
-    mpz_class result;
-    int job_index;
+    // iteration number
+    mpz_class iteration;
 
+    // result of the computation
+    mpz_class result;
+
+    // index of the job in the parent Pohlig-Hellman task
+    int job_index;
+    // pointer to parent Pohlig-Hellman task
     poh_task_t *parent_task;
 };
 
+
+/*
+ * Task structure for one computation of Baby-Step Giant-Step algorithm
+ */
 struct bsgs_task_t {
+    // input parameters
     mpz_class g;
     mpz_class b;
     mpz_class p;
     mpz_class order;
 
+    // computed m for the task
     mpz_class m;
+
+    // result of the computation
     mpz_class result;
 
+    // open addressing hash table
     std::vector<std::atomic_uint64_t> table;
 
-    // pointer to parent Poligh-Hellman task
-    // TODO is this a unique pointer?
+    // pointer to parent Pohlig-Hellman on prime power order groups task
     poh_pp_task_t *parent_task;
 };
--- ff/pohlig_hellman_ff.cpp
+++ ff/pohlig_hellman_ff.cpp
@@ -8,32 +8,40 @@
 #include <algorithm>
 #include <sstream>
 #include <mutex>
-
-
 #include <gmpxx.h>
 
+#include "utimer.h"
 #include "discrete_utils.h"
 #include "oatable.hpp"
-
 #include "dlog_tasks.hpp"
 
 using namespace ff;
 
+#define DEFAULT_LOAD_FACTOR 4.5
 
+/*
+ * Table initialization stage, sets all values in the table to the sentinel value (max uint64_t)
+ */
 struct TableInitializationStage : ff_Map<bsgs_task_t> {
+    int num_workers;
+    TableInitializationStage(int num_workers):num_workers(num_workers) {}
+
     bsgs_task_t *svc(bsgs_task_t *t) {
         parallel_for(0,t->table.size(),[&](const long i) { 
             t->table[i] = std::numeric_limits<uint64_t>::max();
-		}, 1); // TODO nw
+		}, num_workers);
         return t;
     }
 };
 
-// TODO mention in the report that an interface like parallel_for with (start, end) could be useful
-// for tasks like these
+/*
+ * Table construction stage, inserts g^i for i in [0, m) into the table
+ */
 struct TableConstructionStage : ff_Map<bsgs_task_t> {
+    int num_workers;
+    TableConstructionStage(int num_workers):num_workers(num_workers) {}
+
     bsgs_task_t *svc(bsgs_task_t *t) {
-        const int num_workers = 1;
         const int task_granularity = 4;
         size_t chunk_size = t->m.get_ui() / num_workers / task_granularity;
         if (chunk_size == 0) {
@@ -42,27 +50,29 @@ struct TableConstructionStage : ff_Map<bsgs_task_t> {
 
         parallel_for(0,t->m.get_ui(), chunk_size, 1, [&](long start) {
             const long end = std::min(start + chunk_size, t->m.get_ui());
-            // std::cout << "[construction] start: " << start << ", stop: " << end << std::endl;
 
             if (start == 0) {
                 table_insert(t->table, 1, 0);
-                // std::cout << "inserted 0" << std::endl;
                 start += 1;
             }
             mpz_class val = powerMod(t->g, mpz_class(start), t->p);
             for (size_t i = start; i < end; ++i){
                 table_insert(t->table, val, i);
-                // std::cout << "inserted " << val << "i: " << i << std::endl;
                 val = (val * t->g) % t->p;
             }
-        }, num_workers); // TODO nw
+        }, num_workers);
         return t;
     }
 };
 
+/*
+ * Table search stage, searches for b * g^(-im) for i in [0, m) in the table
+ */
 struct TableSearchStage : ff_Map<bsgs_task_t> {
+    int num_workers;
+    TableSearchStage(int num_workers):num_workers(num_workers) {}
+
     bsgs_task_t *svc(bsgs_task_t *t) {
-        const int num_workers = 1;
         const int task_granularity = 4;
         size_t chunk_size = t->m.get_ui() / num_workers / task_granularity;
         if (chunk_size == 0) {
@@ -72,10 +82,8 @@ struct TableSearchStage : ff_Map<bsgs_task_t> {
         std::mutex result_lock;
         std::atomic_bool found = false;
 
-
         parallel_for(0,t->m.get_ui(), chunk_size, 1, [&](long start) {
             const long end = std::min(start + chunk_size, t->m.get_ui());
-            // std::cout << "start: " << start << ", stop: " << end << std::endl;
 
             mpz_class gm = (powerMod(modInversePrime(t->g, t->p), t->m, t->p)) % t->p;
             mpz_class val = (t->b * powerMod(gm, start, t->p)) % t->p;
@@ -86,10 +94,14 @@ struct TableSearchStage : ff_Map<bsgs_task_t> {
                 }
                 std::optional<mpz_class> result = table_search(t->table, val, t->g, t->p);
                 if (result.has_value()) {
+                    // result found, set the result and return
                     std::lock_guard<std::mutex> lock(result_lock);
+
+                    // if the result has already been found, then return
                     if (found) {
                         return;
                     }
+
                     t->result = (i * t->m + result.value()) % t->order;
                     found = true;
                     return;
@@ -97,21 +109,154 @@ struct TableSearchStage : ff_Map<bsgs_task_t> {
                 val = (val * gm) % t->p;
             }
 
-        }, num_workers); // TODO nw
+        }, num_workers);
         return t;
     }
 };
 
-poh_task_t *parse_task() {
+
+bsgs_task_t *create_bsgs_task_from_poh_pp(poh_pp_task_t *t) {
+    bsgs_task_t *bsgs_task = new bsgs_task_t;
+
+    const mpz_class exponent = powerMod(t->factor, t->exponent-1-t->iteration, t->parent_task->p);
+    const mpz_class h_k = powerMod((t->b * powerMod(modInversePrime(t->g, t->parent_task->p), t->result, t->parent_task->p)) % t->parent_task->p, exponent, t->parent_task->p);
+
+    bsgs_task->g = t->gamma;
+    bsgs_task->b = h_k;
+    bsgs_task->p = t->parent_task->p;
+    bsgs_task->order = t->factor;
+    bsgs_task->m = sqrt(bsgs_task->order-1) + 1;
+    bsgs_task->result = mpz_class(-1);
+    bsgs_task->parent_task = t;
+
+    float load_factor = DEFAULT_LOAD_FACTOR;
+    bsgs_task->table = std::vector<std::atomic_uint64_t>((size_t)(bsgs_task->m.get_ui() * load_factor));
+
+    return bsgs_task;
+}
+
+struct Emitter : ff_monode_t<bsgs_task_t> {
+    poh_task_t *pohlig_hellman_task;
+    Emitter(poh_task_t *task):pohlig_hellman_task(task) {}
+
+    bsgs_task_t *svc(bsgs_task_t *input_task) {
+
+        // if there is a task from the feedback channel, just forward it to the farm
+        if (input_task != nullptr) {
+            return input_task;
+        }
+
+        // create all instances of poh_pp_task_t and bsgs_task_t
+        int i=0;
+        for (auto factor : pohlig_hellman_task->order_factorization) {
+            poh_pp_task_t *t = new poh_pp_task_t;
+            t->factor = factor.first;
+            t->exponent = factor.second;
+            t->iteration = 0;
+            t->parent_task = pohlig_hellman_task;
+            t->result = mpz_class(0);
+            t->job_index = i;
+
+            mpz_class p_to_e = powerMod(t->factor, t->exponent, pohlig_hellman_task->p);
+            mpz_class subgroup_order = pohlig_hellman_task->order / p_to_e;
+            mpz_class subgroup_gen = powerMod(pohlig_hellman_task->g, subgroup_order, pohlig_hellman_task->p);
+            mpz_class h = powerMod(pohlig_hellman_task->b, subgroup_order, pohlig_hellman_task->p);
+            t->g = subgroup_gen;
+            t->b = h;
+            t->gamma = powerMod(t->g, powerMod(t->factor, t->exponent-1, pohlig_hellman_task->p), pohlig_hellman_task->p);
+
+            bsgs_task_t *t2 = create_bsgs_task_from_poh_pp(t);
+            ff_send_out(t2);
+            i++;
+
+        }
+
+        return GO_ON;
+    }
+
+    void eosnotify(ssize_t) {
+        // when we receive EOS (from the feedback channel), send EOS to all workers of the farm
+        broadcast_task(EOS);
+        return;
+    }
+};
+
+/*
+ * Collector stage, collects the results of the bsgs tasks, computes the next iterations
+ * and in the end recombines the results using crt
+ */
+struct Collector : ff_monode_t<bsgs_task_t> {
+    bsgs_task_t *svc(bsgs_task_t *t) {
+        // check that the bsgs discrete log has been computed correctly
+        assert(powerMod(t->g, t->result, t->p) == t->b);
+
+
+        if (t->parent_task != nullptr) {
+            // update the parent task result
+            t->parent_task->result += t->result * powerMod(t->parent_task->factor, t->parent_task->iteration, t->p);
+            t->parent_task->result %= t->p;
+
+            // increment the iteration number of parent task
+            t->parent_task->iteration += 1;
+
+            // check if this is the last iteration of the parent task
+            if (t->parent_task->iteration == t->parent_task->exponent) {
+
+                // put the resulting discrete log in the correct position in the general Pohlig
+                // Hellman result vector
+                t->parent_task->parent_task->x[t->parent_task->job_index] = t->parent_task->result;
+                t->parent_task->parent_task->h_i[t->parent_task->job_index] = powerMod(t->parent_task->factor, t->parent_task->exponent, t->p);
+
+                t->parent_task->parent_task->completed_subgroups += 1;
+
+                // check if all subgroups have been computed
+                if (t->parent_task->parent_task->completed_subgroups == t->parent_task->parent_task->order_factorization.size()) {
+                    // combine the results using crt
+                    mpz_class result = crt(t->parent_task->parent_task->x, t->parent_task->parent_task->h_i);
+                    std::cout << "general P-H result: " << result << std::endl;
+
+                    // check heck that we actually found the correct result for the whole task
+                    assert(powerMod(t->parent_task->parent_task->g, result, t->parent_task->parent_task->p) == t->parent_task->parent_task->b);
+
+                    // clean up previously allocated tasks
+                    delete t->parent_task->parent_task;
+                    delete t->parent_task;
+                    delete t;
+
+                    // send EOS to the emitter, terminate all workers
+                    ff_send_out_to(EOS, 0);
+                    return EOS;
+                }
+
+                delete t->parent_task;
+            } else {
+                // not the last iteration of the parent task, compute the next input parameters
+                // for bsgs and send the task to the emitter
+                bsgs_task_t *t2 = create_bsgs_task_from_poh_pp(t->parent_task);
+                ff_send_out_to(t2, 0);
+            }
+        }
+
+        delete t;
+        return GO_ON;
+    }
+};
+
+/*
+ * Parse the input file and create the Pohlig-Hellman task
+ */
+poh_task_t *parse_task(char *filename) {
     poh_task_t *task = new poh_task_t;
 
     std::ifstream myfile;
-    myfile.open("input/pohlig_hellman_inputs.txt");
+    myfile.open(filename);
+
+    if (!myfile.is_open()) {
+        std::cout << "Error opening file" << std::endl;
+    }
 
     std::string line;
 
-    std::getline(myfile, line);
-    std::cout << line << std::endl;
     std::getline(myfile, line);
     std::string sg, sb, sp;
     std::istringstream(line) >> sg >> sb >> sp;
@@ -120,11 +265,10 @@ poh_task_t *parse_task() {
     task->p = mpz_class(sp);
     task->order = task->p - 1;
 
-    // std::cout << "g: " << task->g << ", b: " << task->b << ", p: " << task->p << std::endl;
-
     std::getline(myfile, line);
     auto line_stream = std::istringstream(line);
 
+    // read the factorization of the order of the group
     int size = 0;
     line_stream >> size;
 
@@ -142,162 +286,58 @@ poh_task_t *parse_task() {
     return task;
 }
 
-bsgs_task_t *create_bsgs_task_from_poh_pp(poh_pp_task_t *t) {
-    bsgs_task_t *bsgs_task = new bsgs_task_t;
-
-    const mpz_class exponent = powerMod(t->factor, t->exponent-1-t->iteration, t->parent_task->p);
-    const mpz_class h_k = powerMod((t->b * powerMod(modInversePrime(t->g, t->parent_task->p), t->result, t->parent_task->p)) % t->parent_task->p, exponent, t->parent_task->p);
-
-    bsgs_task->g = t->gamma;
-    bsgs_task->b = h_k;
-    bsgs_task->p = t->parent_task->p;
-    bsgs_task->order = t->factor;
-    bsgs_task->m = sqrt(bsgs_task->order-1) + 1;
-    bsgs_task->result = mpz_class(-1);
-    bsgs_task->parent_task = t;
-
-    float load_factor = 4.0; // TODO
-    bsgs_task->table = std::vector<std::atomic_uint64_t>((size_t)(bsgs_task->m.get_ui() * load_factor));
-
-    return bsgs_task;
-}
-
-struct Emitter : ff_monode_t<bsgs_task_t> {
-    bsgs_task_t *svc(bsgs_task_t *input_task) {
-
-        // forward tasks from the feedback channel
-        if (input_task != nullptr) {
-            // std::cout << "received task: " << input_task->g << std::endl;
-            return input_task;
-        }
-
-        auto task = parse_task();
-
-        // create instances of poh_pp_task_t and bsgs_task_t
-        int i=0;
-        for (auto factor : task->order_factorization) {
-            poh_pp_task_t *t = new poh_pp_task_t;
-            t->factor = factor.first;
-            t->exponent = factor.second;
-            t->iteration = 0;
-            t->parent_task = task;
-            t->result = mpz_class(0);
-            t->job_index = i;
-
-
-            mpz_class p_to_e = powerMod(t->factor, t->exponent, task->p);
-            mpz_class subgroup_order = task->order / p_to_e;
-
-            mpz_class subgroup_gen = powerMod(task->g, subgroup_order, task->p);
-            mpz_class h = powerMod(task->b, subgroup_order, task->p);
-
-            t->g = subgroup_gen;
-            t->b = h;
-
-            t->gamma = powerMod(t->g, powerMod(t->factor, t->exponent-1, task->p), task->p);
-
-            bsgs_task_t *t2 = create_bsgs_task_from_poh_pp(t);
-
-            // std::cout << "g: " << t2->g << ", b: " << t2->b << ", p: " << t2->p << ", m: " << t2->m << std::endl;
-            // std::cout << "m: " << t2->m << std::endl;
-            ff_send_out(t2);
-
-            i++;
-
-        }
-
-        return GO_ON;
-    }
-
-    void eosnotify(ssize_t t) {
-        // when we receive EOS, send EOS to all workers of farm
-        broadcast_task(EOS);
-        return;
-    }
-};
-
-struct Collector : ff_monode_t<bsgs_task_t> {
-    bsgs_task_t *svc(bsgs_task_t *t) {
-        // for (int i=0;i<t->table.size();++i) {
-        //     std::cout << t->table[i] << " ";
-        // }
-        // std::cout << "received " << t->result;
-        // std::cout << " g: " << t->g << ", b: " << t->b << ", p: " << t->p << ", m: " << t->m << std::endl;
-        assert(powerMod(t->g, t->result, t->p) == t->b);
-
-        if (t->parent_task != nullptr) {
-            t->parent_task->result += t->result * powerMod(t->parent_task->factor, t->parent_task->iteration, t->p);
-            t->parent_task->result %= t->p;
-
-            t->parent_task->iteration += 1;
-            if (t->parent_task->iteration == t->parent_task->exponent) {
-                // std::cout << "result: " << t->parent_task->result << std::endl;
-                t->parent_task->parent_task->x[t->parent_task->job_index] = t->parent_task->result;
-                t->parent_task->parent_task->h_i[t->parent_task->job_index] = powerMod(t->parent_task->factor, t->parent_task->exponent, t->p);
-
-                t->parent_task->parent_task->completed_subgroups += 1;
-                // std::cout << "completed subgroups: " << t->parent_task->parent_task->completed_subgroups << std::endl;
-                // std::cout << "order factorization size: " << t->parent_task->parent_task->order_factorization.size() << std::endl;
-                if (t->parent_task->parent_task->completed_subgroups == t->parent_task->parent_task->order_factorization.size()) {
-                    // std::cout << "size of x: " << t->parent_task->parent_task->x.size() << std::endl;
-                    // for(int i=0;i<t->parent_task->parent_task->x.size();++i) {
-                    //     std::cout << "x: " << t->parent_task->parent_task->x[i] << ", h_i: " << t->parent_task->parent_task->h_i[i] << std::endl;
-                    // }
-                    mpz_class result = crt(t->parent_task->parent_task->x, t->parent_task->parent_task->h_i);
-                    std::cout << "general P-H result: " << result << std::endl;
-                    assert(powerMod(t->parent_task->parent_task->g, result, t->parent_task->parent_task->p) == t->parent_task->parent_task->b);
-
-                    delete t->parent_task->parent_task;
-                    delete t->parent_task;
-                    delete t;
-                    // send EOS to the emitter, terminate
-                    ff_send_out_to(EOS, 0);
-                    return EOS;
-                }
-
-                delete t->parent_task;
-            } else {
-                bsgs_task_t *t2 = create_bsgs_task_from_poh_pp(t->parent_task);
-                // std::cout << "sending back to bsgs ";
-                // std::cout << "g: " << t2->g << ", b: " << t2->b << ", p: " << t2->p << ", m: " << t2->m << std::endl;
-                ff_send_out_to(t2, 0);
-            }
-        }
-
-        delete t;
-        return GO_ON;
-    }
-};
-
 int main(int argc, char * argv[]) {
 
+    if (argc != 6) {
+        std::cout << "Usage: ./main <input_filename> <n_bsgs_farms> <nw_init> <nw_construction> <nw_search>" << std::endl;
+        return 1;
+    }
 
-    const int n_farms = 3; // TODO
+    char *filename = argv[1];
+    int n_farms = atoi(argv[2]);
+    int nw_init = atoi(argv[3]);
+    int nw_construction = atoi(argv[4]);
+    int nw_search = atoi(argv[5]);
 
+
+    // create the bsgs workers
     std::vector<ff_node *> bsgs_instances;
     for (int i=0; i<n_farms; ++i) {
         auto worker = new ff_Pipe<bsgs_task_t>(
-        make_unique<TableInitializationStage>(), // TODO remember to add nw
-        make_unique<TableConstructionStage>(),
-        make_unique<TableSearchStage>());
+        make_unique<TableInitializationStage>(nw_init),
+        make_unique<TableConstructionStage>(nw_construction),
+        make_unique<TableSearchStage>(nw_search));
         bsgs_instances.push_back(worker);
     }
 
-    Emitter e;
+    Emitter e(parse_task(filename));
     Collector c;
 
+    // create the farm
     ff_farm bsgs_farm;
     bsgs_farm.add_emitter(&e);
     bsgs_farm.add_workers(bsgs_instances);
     bsgs_farm.add_collector(&c);
+
+    // set scheduling on demand, because the tasks are few
+    // this could lead to better load balancing
+    bsgs_farm.set_scheduling_ondemand();
     bsgs_farm.cleanup_workers();
 
+    // create feedback channel
     bsgs_farm.wrap_around();
 
-    if (bsgs_farm.run_and_wait_end()<0) {
-        error("running pipe");
-        return -1;
+    long time_taken;
+    {
+        utimer timer("ff", &time_taken);
+
+        if (bsgs_farm.run_and_wait_end()<0) {
+            error("running pipe");
+            return -1;
+        }
     }
+
+    std::cout << "Found result in " << time_taken << " microseconds" << std::endl;
 
     return 0;
 }
